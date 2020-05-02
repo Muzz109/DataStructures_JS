cdb4672874e68ebd9510baed4b8d4c01a1da577f
Muzz109 <Mujtaba.109.as@gmail.com>
2020-05-02 23:33:56 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+function findFactorialRecursive(number){
+
+if(number==2){
+  return 2
+}
+
+  return number *findFactorialRecursive(number-1);
+}
+//findFactorialRecursive(5)
+
+
+function findfctoriteratively(num){
+
+ if(num === 2){
+
+   return 2;
+ }
+ for(i=2; i <= number;i++){
+   answer=answer*i;
+ }
+ 
+ return answer;
+ 
+}
+
